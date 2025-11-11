11c2f418c4da4ef037d64d6d895ba5f2c86444bb
Itaysc23 <itay.schwartz@post.runi.ac.il>
2025-11-11 12:42:28 +0200

    hw2

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,17 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    int N = Integer.parseInt(args[0]);
+		double sum = 0.0;
+		int sign = 1; // חיוביות ושליליות של טור לייבניץ 
+		 for (int i = 0; i < N; i++){
+			double denominator = 2.0 * i + 1.0; //חישוב המכנה שתמיד יהיה קשור ב2
+			sum = sum + sign * (1.0 / denominator); // חישוב הסכום הכללי
+			sign = -sign;
+		 }
+		 double piApproximated = sum * 4.0;
+		 System.out.println("pi according to Java: " + Math.PI);
+		 System.out.println("pi, approximated:");
+		 System.out.println(piApproximated);
 	}
 }
